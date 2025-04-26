7563e5509b34a953402390c9bc72645749efc1e4
Hadi Roshan <HadiRoshanNDS@gmail.com>
2025-04-26 16:39:09 +0330

    Lesson 39 commit

---
+++ section4OOP/ls38/ls38.js
@@ -0,0 +1,15 @@
+"use strict";
+class Ride {
+    start() {
+        Ride.activeRides++;
+    }
+    stop() {
+        Ride.activeRides--;
+    }
+}
+Ride.activeRides = 0;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+console.log(Ride.activeRides);
