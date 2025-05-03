f65b18cfe770c333d4bf0174f572fc5f1667bbc9
Hadi Roshan <HadiRoshanNDS@gmail.com>
2025-05-03 09:41:05 +0330

    Lesson 53 Commit (Type Mapping)

--- section5Generics/ls51.ts
+++ section5Generics/ls51.ts
@@ -9,26 +9,35 @@ class Store<T> {
   add(obj: T): void {
     this._objects.push(obj);
   }
+
+  find(property: string, value: unknown): T | undefined {
+    return this._objects.find((obj) => obj[property] === value);
+  }
 }
+
+let store = new Store<Product>();
+store.add({ name: "a", price: 1 });
+store.find("name", "a");
+store.find("price", 1);
 
 //Pass on the generic type parameter
-class CompressibleStore<T> extends Store<T> {
-  compress() {}
-}
+// class CompressibleStore<T> extends Store<T> {
+//   compress() {}
+// }
 
-// let store = new CompressibleStore<Product>();
-// store.compress();
+// // let store = new CompressibleStore<Product>();
+// // store.compress();
 
-//Restrict the generic type parameter
-class SearchableStore<T extends { name: string }> extends Store<T> {
-  find(name: string): T | undefined {
-    return this._objects.find((obj) => obj.name === name);
-  }
-}
+// //Restrict the generic type parameter
+// class SearchableStore<T extends { name: string }> extends Store<T> {
+//   find(name: string): T | undefined {
+//     return this._objects.find((obj) => obj.name === name);
+//   }
+// }
 
-//Fix the generic type parameter
-class ProductStore extends Store<Product> {
-  filterByCategory(category: string): Product[] {
-    return [];
-  }
-}
+// //Fix the generic type parameter
+// class ProductStore extends Store<Product> {
+//   filterByCategory(category: string): Product[] {
+//     return [];
+//   }
+// }
