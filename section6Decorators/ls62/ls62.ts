4bb26684d3731bac63f9d521851cbcac5933866e
Hadi Roshan <HadiRoshanNDS@gmail.com>
2025-05-08 11:32:03 +0330

    Lesson 67 commit

---
+++ section6Decorators/ls62/ls62.ts
@@ -0,0 +1,19 @@
+type WatchedParameter = {
+  methodName: string;
+  parameterIndex: number;
+};
+
+const WatchedParameters: WatchedParameter[] = [];
+
+function Watch(target: any, methodName: string, parameterIndex: number) {
+  WatchedParameters.push({
+    methodName,
+    parameterIndex,
+  });
+}
+
+class Vehicle {
+  move(@Watch speed: number) {}
+}
+
+console.log(WatchedParameters);
